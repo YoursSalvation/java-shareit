0affca22764c5967c7afd09f9619c8302b56bf15
Dmitry Prokhorov <prokhorovds26@mail.ru>
2025-11-13 18:27:07 +0500

    add some tests

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,13 +1,58 @@
 package ru.practicum.shareit;
 
-import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.test.annotation.DirtiesContext;
+import ru.practicum.shareit.item.api.ItemResponseDto;
+import ru.practicum.shareit.user.api.UserResponseDto;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@AutoConfigureTestDatabase
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ShareItTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    protected TestRestTemplate restTemplate;
+
+    @Value("${shareit.api.auth.userheader}")
+    private String userIdHeader;
+
+    protected boolean checkPost(String endPoint, String json, int code, String search) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> request = new HttpEntity<>(json, headers);
+        ResponseEntity<String> response = restTemplate.exchange(endPoint, HttpMethod.POST, request, String.class);
+        if (code != response.getStatusCode().value()) return false;
+        if (search == null) return true;
+        if (response.getBody() == null) return false;
+        return response.getBody().contains(search);
+    }
+
+    protected Long makeUserAndReturnId(String json) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> request = new HttpEntity<>(json, headers);
+        ResponseEntity<UserResponseDto> response = restTemplate.exchange("/users", HttpMethod.POST, request,
+                UserResponseDto.class);
+        assertNotNull(response.getBody());
+        return response.getBody().getId();
+    }
+
+    protected Long makeItemAndReturnId(String json, Long userId) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        if (userId != null) headers.set(userIdHeader, String.valueOf(userId));
+        HttpEntity<String> request = new HttpEntity<>(json, headers);
+        ResponseEntity<ItemResponseDto> response = restTemplate.exchange("/items", HttpMethod.POST, request,
+                ItemResponseDto.class);
+        assertNotNull(response.getBody());
+        return response.getBody().getId();
+    }
 
 }
